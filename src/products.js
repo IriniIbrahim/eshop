5aee11bd67bb5b095e9a427d5b9a3a04c4fd359a
IriniIbrahim <iriniibrahim3@gmail.com>
2023-05-20 20:48:37 +0200

    adding1

---
+++ src/products.js
@@ -0,0 +1,38 @@
+import product1 from './assets/1.jpg'
+import product2 from './assets/2.jpg'
+import product3 from './assets/3.jpg'
+import product4 from './assets/4.jpg'
+import product5 from './assets/5.jpg'
+
+export const PRODUCTS = [
+	{
+		id: 1,
+		productName: "Pullover",
+		price: 99,
+		productImage: product1,
+	},
+	{
+		id: 2,
+		productName: "Sneakers",
+		price: 150,
+		productImage: product2,
+	},
+	{
+		id: 3,
+		productName: "Green Blazer",
+		price: 400,
+		productImage: product3,
+	},
+	{
+		id: 4,
+		productName: "Bag",
+		price: 300,
+		productImage: product4,
+	},
+	{
+		id: 5,
+		productName: "Blue Blazer",
+		price: 400,
+		productImage: product5,
+	}
+]
