d03f299d773cb5c1d3de66ac11fb26dd6a22d8aa
Irini Ibrahim <iriniibrahim3@gmail.com>
2023-05-30 15:23:19 +0200

    adding login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop"
 import { Cart } from "./pages/cart/cart"
 import { ShopContextProvider } from './context/shop-context';
+import { Login } from './pages/authorize/login';
+import { Register } from './pages/authorize/register';
 
 function App() {
+
+
   return (
     <div className="App">
+
       <ShopContextProvider>
         <Router>
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </Router>
       </ShopContextProvider>
+
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
+
 export const Navbar = () => {
 	return (
 		<div className="navbar">
@@ -10,6 +11,8 @@ export const Navbar = () => {
 				<Link to="/cart">
 					<ShoppingCart size={32} />
 				</Link>
+				<Link to="/login"> Login </Link>
+				{/* <Link to="/register"> Register </Link> */}
 			</div>
 		</div>
 	);
